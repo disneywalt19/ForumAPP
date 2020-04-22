7567e24573f9762327b8f16989012e3d6c9dfea4
disneywalt19 <disneydj19@gmail.com>
2020-04-22 20:08:33 +0200

    ForumAPP version-5 date: 22-04-2020 20:07 end of part 58

--- app/Http/Controllers/DiscussionsController.php
+++ app/Http/Controllers/DiscussionsController.php
@@ -4,25 +4,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Discussion;
+use App\Http\Requests\CreateDiscussionRequest;
 
-
-use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Str;
 
 
 
 
-class DiscussionsController extends Controller
-{
+
+
+class DiscussionsController extends Controller {
+	
+	public function __construct() {
+		
+		$this->middleware('auth')->only(['create', 'store']);
+		
+	}
+	
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+     */ 
+	
+    public function index() {
+		
+		return view('discussions.index', [
+			
+			'discussions' => Discussion::paginate(5)
+			
+		]);
+        
     }
 
     /**
@@ -30,6 +45,7 @@ class DiscussionsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+	
     public function create() {
         
 		return view('discussions.create');
@@ -43,9 +59,25 @@ class DiscussionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+	
+    public function store(CreateDiscussionRequest $request) {
+		
+		auth()->user()->discussions()->create([
+			
+			'title' => $request->title,
+			
+			'slug' => Str::slug($request->title),
+			
+			'content' => $request->content,
+			
+			'channel_id' => $request->channel
+			
+		]);
+		
+		session()->flash('success', 'Discussion posted."');
+		
+		return redirect()->route('discussion.index');
+        
     }
 
     /**
